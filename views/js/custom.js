28bda72ccf8e110834828c49bb116faad5f1a58d
minic studio <antal.levente@minic.ro>
2013-04-10 12:05:47 +0300

    made to be multi functional

--- views/js/custom.js
+++ views/js/custom.js
@@ -1,9 +1,8 @@
 $(document).ready(function(){
 	$('.list-selector').change(function(){
+		var list = $(this).attr('data-list');
 		var id = $(this).find('option:selected').attr('value');
 		$('.visible-list').hide().removeClass('visible-list');
-		$('#'+id).show().addClass('visible-list');
-
-		
+		$('#'+list+'-'+id).show().addClass('visible-list');	
 	});
 });
