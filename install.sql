5137e477a85da8fac87193d69e5cf71e45bb5c69
minic studio <antal.levente@minic.ro>
2013-04-11 15:35:28 +0300

    remove

--- install.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `PREFIX_minicskeleton` (
-  `id_skeleton` int(10) unsigned NOT NULL,
-  `text` text,
-  PRIMARY KEY (`id_skeleton`)
-) ENGINE=ENGINE_TYPE DEFAULT CHARSET=utf8;
-
-INSERT INTO `PREFIX_minicskeleton` VALUES (1, 'Some text');
